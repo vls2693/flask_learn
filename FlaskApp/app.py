806db49fbc23a9988dc7bc618d3e5c22655afc71
vls2693 <belyakov38@gmail.com>
2019-01-11 22:10:40 +0300

    2

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 
 if __name__ == "__main__":
     app.run()
